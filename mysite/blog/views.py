ff29f5e87c24b0f260f548473d22042f882846ff
yy <yuanye@iapppay.com>
2018-11-28 21:31:20 +0800

    1、admin后台增加博客管理板块

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
+from .models import BlogType, Blog
 
 # Create your views here.
+
+def blog_list(request):
+    return HttpResponse("hello World!")

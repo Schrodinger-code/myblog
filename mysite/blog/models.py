3ab7f8452af5ef6286aac4caadc96781b87c96b5
yy <yuanye@iapppay.com>
2018-12-29 17:30:52 +0800

    1、创建博客主页以及美化； 2、增加博客列表、博客分类、博客详情页等页面

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class BlogType(models.Model):
     type_name = models.CharField(max_length=15, verbose_name="博客分类")
 
